5f849c7cd3ac336b9cfd3c070e840c5bcc70bca5
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-02-12 14:03:12 -0300

    :wrench: fix: Fixing select, now they all are working

--- read_table.py
+++ read_table.py
@@ -17,7 +17,10 @@ def read_table():
         "Do you want to:\n"
         "1. Print all books info\n"
         "2. Search for a book by Title\n"
-        "3. List all books by Genre, author, format, or Language?"
+        "3. List all books by Genre, author, format, or Language?\n"
+        "4. Print all Authors\n"
+        "5. Print all Genres\n"
+        "6. Print all Languages\n"
     )
     answer = input()
 
@@ -48,26 +51,19 @@ def read_table():
 
     if answer == "2":
         book_title = input("Enter the title of the book: ")
+    
         cursor.execute(
-            f"SELECT title, author, genre, "
-            f"start_date, end_date, rating, pages, language, format "
-            f"FROM {TABLE_NAME} "
-            f"WHERE title=?",
+            f"SELECT {TABLE_NAME}.title, {AUTHORS_TABLE}.name, "
+            f"{GENRES_TABLE}.name, start_date, end_date, rating, pages, "
+            f"{LANGUAGES_TABLE}.name "
+            f"FROM {TABLE_NAME}, {AUTHORS_TABLE}, "
+            f"{GENRES_TABLE}, {LANGUAGES_TABLE} "
+            f"WHERE {TABLE_NAME}.id_author = {AUTHORS_TABLE}.id "
+            f"AND {TABLE_NAME}.id_genre = {GENRES_TABLE}.id "
+            f"AND {TABLE_NAME}.id_language = {LANGUAGES_TABLE}.id "
+            f"AND {TABLE_NAME}.title = ?",
             (book_title,),
         )
-
-        # cursor.execute(
-        #     f"SELECT {TABLE_NAME}.title, {AUTHORS_TABLE}.name, "
-        #     f"{GENRES_TABLE}.name, start_date, end_date, rating, pages, "
-        #     f"{LANGUAGES_TABLE}.name "
-        #     f"FROM {TABLE_NAME}, {AUTHORS_TABLE}, "
-        #     f"{GENRES_TABLE}, {LANGUAGES_TABLE} "
-        #     f"WHERE {TABLE_NAME}.id_author = {AUTHORS_TABLE}.id "
-        #     f"AND {TABLE_NAME}.id_genre = {GENRES_TABLE}.id "
-        #     f"AND {TABLE_NAME}.id_language = {LANGUAGES_TABLE}.id "
-        #     f"AND {TABLE_NAME}.title = ?",
-        #     (book_title,),
-        # )
 
         book = cursor.fetchone()
         if book:
@@ -88,10 +84,16 @@ def read_table():
         if choice == "1":
             genre = input("Enter the genre: ")
             cursor.execute(
-                f"SELECT title, author, genre, "
-                f"start_date, end_date, rating, pages, language, format "
-                f"FROM {TABLE_NAME} "
-                f"WHERE genre=?",
+                f"SELECT {TABLE_NAME}.title, {AUTHORS_TABLE}.name, "
+                f"{GENRES_TABLE}.name, {TABLE_NAME}.start_date, "
+                f"{TABLE_NAME}.end_date, {TABLE_NAME}.rating, "
+                f"{TABLE_NAME}.pages, {LANGUAGES_TABLE}.name "
+                f"FROM {TABLE_NAME}, {AUTHORS_TABLE}, {GENRES_TABLE}, "
+                f"{LANGUAGES_TABLE} "
+                f"WHERE {TABLE_NAME}.id_author = {AUTHORS_TABLE}.id "
+                f"AND {TABLE_NAME}.id_genre = {GENRES_TABLE}.id "
+                f"AND {TABLE_NAME}.id_language = {LANGUAGES_TABLE}.id "
+                f"AND {GENRES_TABLE}.name = ?",
                 (genre,),
             )
             books = cursor.fetchall()
@@ -114,10 +116,16 @@ def read_table():
         if choice == "2":
             author = input("Enter the author: ")
             cursor.execute(
-                f"SELECT title, author, genre, "
-                f"start_date, end_date, rating, pages, language, format "
-                f"FROM {TABLE_NAME} "
-                f"WHERE author=?",
+                f"SELECT {TABLE_NAME}.title, {AUTHORS_TABLE}.name, "
+                f"{GENRES_TABLE}.name, {TABLE_NAME}.start_date, "
+                f"{TABLE_NAME}.end_date, {TABLE_NAME}.rating, "
+                f"{TABLE_NAME}.pages, {LANGUAGES_TABLE}.name "
+                f"FROM {TABLE_NAME}, {AUTHORS_TABLE}, {GENRES_TABLE}, "
+                f"{LANGUAGES_TABLE} "
+                f"WHERE {TABLE_NAME}.id_author = {AUTHORS_TABLE}.id "
+                f"AND {TABLE_NAME}.id_genre = {GENRES_TABLE}.id "
+                f"AND {TABLE_NAME}.id_language = {LANGUAGES_TABLE}.id "
+                f"AND {AUTHORS_TABLE}.name = ?",
                 (author,),
             )
             books = cursor.fetchall()
@@ -139,10 +147,16 @@ def read_table():
         if choice == "3":
             book_format = input("Enter the format: ")
             cursor.execute(
-                f"SELECT title, author, genre, "
-                f"start_date, end_date, rating, pages, language, format "
-                f"FROM {TABLE_NAME} "
-                f"WHERE format=?",
+                f"SELECT {TABLE_NAME}.title, {AUTHORS_TABLE}.name, "
+                f"{GENRES_TABLE}.name, {TABLE_NAME}.start_date, "
+                f"{TABLE_NAME}.end_date, {TABLE_NAME}.rating, "
+                f"{TABLE_NAME}.pages, {LANGUAGES_TABLE}.name "
+                f"FROM {TABLE_NAME}, {AUTHORS_TABLE}, {GENRES_TABLE}, "
+                f"{LANGUAGES_TABLE} "
+                f"WHERE {TABLE_NAME}.id_author = {AUTHORS_TABLE}.id "
+                f"AND {TABLE_NAME}.id_genre = {GENRES_TABLE}.id "
+                f"AND {TABLE_NAME}.id_language = {LANGUAGES_TABLE}.id "
+                f"AND {TABLE_NAME}.id_format = ?",
                 (book_format,),
             )
             books = cursor.fetchall()
@@ -166,10 +180,16 @@ def read_table():
         if choice == "4":
             language = input("Enter the language: ")
             cursor.execute(
-                f"SELECT title, author, genre, "
-                f"start_date, end_date, rating, pages, language, format "
-                f"FROM {TABLE_NAME} "
-                f"WHERE language=?",
+                f"SELECT {TABLE_NAME}.title, {AUTHORS_TABLE}.name, "
+                f"{GENRES_TABLE}.name, {TABLE_NAME}.start_date, "
+                f"{TABLE_NAME}.end_date, {TABLE_NAME}.rating, "
+                f"{TABLE_NAME}.pages, {LANGUAGES_TABLE}.name "
+                f"FROM {TABLE_NAME}, {AUTHORS_TABLE}, {GENRES_TABLE}, "
+                f"{LANGUAGES_TABLE} "
+                f"WHERE {TABLE_NAME}.id_author = {AUTHORS_TABLE}.id "
+                f"AND {TABLE_NAME}.id_genre = {GENRES_TABLE}.id "
+                f"AND {TABLE_NAME}.id_language = {LANGUAGES_TABLE}.id "
+                f"AND {LANGUAGES_TABLE}.name = ?",
                 (language,),
             )
             books = cursor.fetchall()
@@ -186,5 +206,20 @@ def read_table():
                 print(tabulate(table_data, headers=headers, tablefmt="grid"))
             else:
                 print('Language not found')
+    if answer == "4":
+        cursor.execute(f"SELECT * FROM {AUTHORS_TABLE}")
+        authors = cursor.fetchall()
+        print(tabulate(authors, headers=["id", "Author"], tablefmt="grid"))
+
+    if answer == "5":
+        cursor.execute(f"SELECT * FROM {GENRES_TABLE}")
+        genres = cursor.fetchall()
+        print(tabulate(genres, headers=["id", "Genre"], tablefmt="grid"))
+
+    if answer == "6":
+        cursor.execute(f"SELECT * FROM {LANGUAGES_TABLE}")
+        languages = cursor.fetchall()
+        print(tabulate(languages, headers=["id", "Language"], tablefmt="grid"))
+
     cursor.close()
     connection.close()
